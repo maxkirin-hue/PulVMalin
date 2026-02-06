4a06418c8fbf4ba4b739e09e5677f1d0ded5c0a0
maxkirin-hue <maxnicolet@gmail.com>
2026-02-06 09:55:05 +0100

    Create state.ts

---
+++ src/state/state.ts
@@ -0,0 +1,20 @@
+export const state = {
+  machineType: null,
+  machineName: "",
+  familyKey: null,
+  modelKey: null,
+  dose: null,
+  largeur: null,
+  vitesse: null,
+  arboCount: null,
+  rampeCount: null,
+  forced: false,
+
+  forcedNozzleValue: "",
+  forcedNozzleValueGroup1: "",
+  forcedNozzleValueGroup2: "",
+
+  results: [],
+  qTotal: 0,
+  recommendedPressure: 0,
+};
