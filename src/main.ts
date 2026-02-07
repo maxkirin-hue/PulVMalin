1a6acbaa5480de36ce2d135a2a55c95c8a724bae
maxkirin-hue <maxnicolet@gmail.com>
2026-02-07 17:59:15 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { initMachineButtons } from "./ui/machine";
 import { initNavigation } from "./ui/navigation";
+import "./ui/forms";
 
-document.addEventListener("DOMContentLoaded", () => {
-  initNavigation();
-  initMachineButtons();
-});
+initNavigation();
+initMachineButtons();
