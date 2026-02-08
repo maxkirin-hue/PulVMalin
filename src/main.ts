afc2b213388189d5028a2496f6ecccd97dba9e76
maxkirin-hue <maxnicolet@gmail.com>
2026-02-08 11:10:42 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { initMachineButtons } from "./ui/machine";
 import { initNavigation } from "./ui/navigation";
 import "./ui/forms";
+import { setupInstallButton, showIosInstallBanner } from "./pwa/install";
 
+setupInstallButton();
+showIosInstallBanner();
 initNavigation();
 initMachineButtons();
